7ed562019f75c573305e0803ba4c073920366cba
GALLETAXO <galletoelpro@outlook.com>
2023-09-30 21:39:27 -0600

    codigoTareaArchivosCompletado

--- ProgramacionAvanzada/2doParcial/Lector/Program.Functions.cs
+++ ProgramacionAvanzada/2doParcial/Lector/Program.Functions.cs
@@ -7,28 +7,32 @@ using System.Security.Cryptography.X509Certificates;
 partial class Program
 {
 
-    static string  busqueda(string dir)
+    static string  Busqueda(string dir) // esta funcion se encarga de buscar el archivo de texto que agregaste, en base a su nombre, el cual deberas proporcionarle
     {
+        string textFile; // aqui guardaremos la direccion del archivo de texto
         WriteLine("Ingresa el nombre del documento incluyendo su extension y presiona ENTER");
-        String? input = ReadLine();
+        String? input = ReadLine(); // en esta parte solicitamos y recibimos el nombre junto con la extension del archivo de texto a revisar
+        
         try
         {
-            
-            string textFile = Combine (dir, input);
-            if (textFile.Equals(dir))
+            if(string.IsNullOrEmpty(input)) // en caso de no haberse ingresado nada, regresaremos un valor que se tomara como incorrecto para volver a intentar la busqueda
             {
                 WriteLine("No ingresaste ningun nombre, intentalo de nuevo, ENTER para continuar");
                 ReadLine();
                 return "noup";
-            }
 
-            if(Path.Exists(textFile))
+            }
+            else // si recibimos un dato, este lo agregaremos a la direccion que ya teniamos de nuestra carpeta
+            {
+                textFile = Combine (dir, input);
+            }
+            if(Path.Exists(textFile))// posteriormente revisaremos si ese nombre ingresa efectivamente existe 
             {
                 WriteLine("Archivo encontrado!!!");
-                return textFile;
+                return textFile; // en caso positivo regresaremos la direccion completa del archivo de texto
 
             }
-            else
+            else // en el caso opuesto indicaremos que no se encontro el archivo y pasaremos un noup en señal de que se debe volver a intentar la busqueda
             {
                 WriteLine("Fracaso, intentalo de nuevo, ENTER para continuar");
                 ReadLine();
@@ -38,8 +42,8 @@ partial class Program
             }
             
         }
-        catch(OverflowException)
-        {
+        catch(OverflowException) // puesto que estoy pidiendo al usuario que ingrese un valor,
+        {                        //prefiero tomar precausiones y controlar algunas exepciones que igualmente reintentaran la busqueda
             WriteLine("Es un dato muy muy grande!!!");
             WriteLine("Fracaso, intentalo de nuevo, ENTER para continuar");
             ReadLine();
@@ -54,7 +58,7 @@ partial class Program
             return "noup";
 
         }
-        catch (Exception ex)
+        catch (Exception ex) // esta es una exepcion mas general por si llega a ocurrir algo no contemplado anteriormente
         {
             WriteLine($"Oh Nooooo!!!  {ex.GetType} says {ex.Message}");
             WriteLine("Fracaso, intentalo de nuevo, ENTER para continuar");
@@ -67,42 +71,85 @@ partial class Program
 
     }
 
-    static string Vocales(string texto, char letra)
+
+    static int Vocales( ref string texto, char letra) // en esta funcion realizamos la busqueda de las vocales repetidas
     {
         int cont = 0, punto = 0;
 
-        for(int i = 0; i < texto.Length; i++)
+        for(int i = 0; i < texto.Length; i++) // revisaremos cada espacion del texto que obtuvimos 
         {
 
-            if(texto[i].Equals(letra))
+            if(texto[i].Equals(letra)) // si una letra del texto coinside con la vocal que buscamos...
             {
         
-                cont++;
-                punto = i;
+                cont++; // aumentara el contador
+                punto = i; // y guardaremos su ubicacion para en el caso de que sea la ultima, poder reemplazarla con la cuenta que llevos de la vocal 
             }
         }
-        if (cont > 0)
+        if (cont > 0) // en caso de haber encontrado aun que sea 1 vocal 
         {
 
-            texto = texto.Insert(punto, cont.ToString());
-            cont = 0;
-            for(int i = 0; i < texto.Length; i++)
-            {
-
-                if(texto[i].Equals(letra))
-                {
-
-                    cont++;
-                    punto = i;
-                }
+            texto = texto.Insert(punto+1, cont.ToString()); // buscaremos la ultima vocal gracias a que guardamos su ubicacion y agregaremos la cuentaa un lado
+            texto = texto.Remove(punto, 1); // posteriormente eliminaremos la vocal 
+            // puesto que es muy probable que utilicemos numeros de mas de 1 cifra, es mas conveniente insertar el numero a un lado conocido, para despues borrar la letra
+            
         }
 
-        texto = texto.Remove(punto, 1);
-        WriteLine(cont);
-        }
-         return texto;
+        WriteLine($"{letra} - {cont}"); // finzalizamos mostrando en consola el numero de vocales encontrado, solo por comodidad
+        return cont; // regresaremos la cantidad de ejemplares encontrados de la vocal especificada
 
     }
+
+
+    static string Palindromear(string texto) // aqui realizaremos la busqueda del palindromo mas largo de todo el texto
+    {
+        string pal = ""; // creamos un string vacio para malvadamente ir agregando letra por letra cada palabra hasta encontra algo que no sea letra
+        string bigpal = ""; // aqui iremos guardando los palindromos en caso de que sean mas grandes que el que ya habiamos encontrado
+        int bigtam = 0; // el tamaño del palindromo mas grande, si ya se, ya era un poco tarde y se me hacia mas comodo asi jajaja
+
+
+        for(int i = 0; i< texto.Length; i++) // revisaremos letra por letra el texto 
+        {
+            if(texto[i] > 96 && texto[i] < 123) // si lo que tiene en esa posicion el texto es una letra...
+            {
+              pal += texto[i]; // lo agregaremos a la palabra
+            }
+        else  // si no es una letra, significa que acabo nuestra palabra, por lo que podemos analizarla
+        {
+            
+            int st = 0; // esta variable indica el inicio de la palabra
+            bool def = true; 
+            for(int j = pal.Length -1; j >= 0; j--) // revisamos cada letra
+            {
+                if(pal[st] == pal[j]) // si la primera y ultima letra son iguales seguiremos, revisando cada contraparte
+                {
+                    st++;
+                }
+                else // en el caso contrario...
+                {
+                    def = false; // definimos que esta palabra no es un palindromo y no la tomaremos en cuenta
+                    break; // acabamos el for, ya que ya determinamos que no es un palindromo 
+
+                }
+           }
+
+           if(def == true && pal.Length > bigtam ) // en esta parte, primero revisamos que la palabra ya sea validada como palindromo y 
+            {                                      //luego revisamos si mide mas que nuestro anteior palindromo mas grande
+                
+                bigpal = pal; // si estas condiciones se cumplen, cambiaremos nuestro palindromo mas grande
+               bigtam = pal.Length;
+
+            }
+    
+           pal = ""; // luego borraremos la palabra 
+        
+        }
+        }
+        return bigpal; // al finalizar de revisar cada palabra, regresaremos el palindromo mas grande
+
+
+    }
+
 
     
 
--- ProgramacionAvanzada/2doParcial/Lector/Program.cs
+++ ProgramacionAvanzada/2doParcial/Lector/Program.cs
@@ -5,46 +5,69 @@ using static System.IO.Directory;
 using static System.IO.Path; 
 using static System.Environment;
 using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.VisualBasic.FileIO;
 //primero las librerias para todo el  manejo y uso de direcciones y directorios :p
 
 
 
-string AgregaTuTexto = Combine(GetFolderPath(SpecialFolder.MyDocuments), "AgregaTuTexto"), dir;
+string AgregaTuTexto = Combine(GetFolderPath(SpecialFolder.MyDocuments), "AgregaTuTexto"), dir;// empezamos armando la direccion donde ira el archivo de texto
 WriteLine("Hola apreciado usuario, ve preparando un archivo de texto!!!");
-WriteLine($"Se ha creado una nueva carpeta para que agregues ahi tu archivo {AgregaTuTexto}");
-CreateDirectory(AgregaTuTexto);
-
 if(Path.Exists(AgregaTuTexto))
 {
-    WriteLine($"Se ha creado una nueva carpeta para que agregues ahi tu archivo {AgregaTuTexto}");
+    WriteLine($" La carperta ay se habia creado anteriormente, esta en: {AgregaTuTexto}"); // No hace falta crear la carpeta puesto que ya existe
 }
 else
 {
-    WriteLine("Oh no, un desafortunado error ha ocurrido");
-
+    CreateDirectory(AgregaTuTexto); // creamos la carpeta en la direccion especificada anteriormente
+     
 }
-
-dir = busqueda(AgregaTuTexto);
-
-while(dir.Equals("noup"))
+if(Path.Exists(AgregaTuTexto))
 {
-    dir = busqueda(AgregaTuTexto);
+    WriteLine($"Se ha creado una nueva carpeta para que agregues ahi tu archivo {AgregaTuTexto}");// le decimos la ubicacion de la carpeta donde debe agregar su archivo de texto 
+}
+else
+{
+    WriteLine("Oh no, un desafortunado error ha ocurrido"); // un mensaje de error por si las moscas
+    Environment.Exit(0); 
+}
+
+dir = Busqueda(AgregaTuTexto); // llamamos a esta funcion para que busque el archivo de texto que agregaste 
+
+while(dir.Equals("noup")) // en esta parte detectaremos si la busqueda fue un fracaso, para en ese caso repetirla
+{
+    dir = Busqueda(AgregaTuTexto);
 }
 
 
-StreamReader textReader = File.OpenText(dir);
-string texto = textReader.ReadToEnd();
-textReader.Close();
-texto = texto.ToLower();
-WriteLine(texto);
+StreamReader textReader = File.OpenText(dir); // ya que tenemos la direccion correcta, empezaremos con la lectura del archivo 
+string texto = textReader.ReadToEnd(); // ingresaremos el archivo completo a un int, de esta forma lo analizaremos mas comodamente 
+textReader.Close(); // cerramos la lectura
+// en esta parte hacemos algunas "correcciones" al texto, mas que nada para que sea mas facil trabajar con el 
+texto = texto.ToLower(); // hacemos que todo el texto este en minusculas 
+texto = Regex.Replace(texto.Normalize(NormalizationForm.FormD), @"[\u0300-\u0301-\u0302]+", ""); // borramos los acentos, y la ^ buscandolos directamente por su unicode para manejar mejor el texto
+texto = Regex.Replace(texto.Normalize(NormalizationForm.FormD), @"[\u0308]+", ""); // hacemos lo mismo con la dieresis 
 
-texto = Vocales(texto, 'a');
-texto = Vocales(texto, 'e');
-texto = Vocales(texto, 'i');
-texto = Vocales(texto, 'o');
-texto = Vocales(texto, 'u');
+StreamWriter textWriter = File.CreateText(dir); // empezamos con la edicion del archivo de texto, en el orden que se va mencionando en la asignacion
+textWriter.WriteLine($"a - {Vocales(ref texto, 'a')}"); // en esta parte escribimos en el archivo cuantos ejemplares fuimos encontrando de cada vocal
+textWriter.WriteLine($"e - {Vocales(ref texto, 'e')}"); // ya que le pasamos el texto por referencia, en la funcion le hacemos cambios a este mismo texto
+textWriter.WriteLine($"i - {Vocales(ref texto, 'i')}"); // de esta forma la funcion puede usarse para regresar la cantidad de repeticiones de una vocal
+textWriter.WriteLine($"o - {Vocales(ref texto, 'o')}"); // y de paso escribir en la ultima  ejemplar de cada vocal ese mismo numero
+textWriter.WriteLine($"u - {Vocales(ref texto, 'u')}");
+textWriter.WriteLine("");
+textWriter.WriteLine(texto); // volovemos a escribir en el archivo el texto ya modificado
 
+string bigpal = Palindromear(texto); // por ultimo hacemos la busqueda del palindromo, pasando el texto 
+if(bigpal == string.Empty || bigpal.Length < 2)
+{
+    textWriter.WriteLine("No se encontro ningun Palindromo :( "); // en el caso de que lo que regrese este vacio, significa que no encontro palindromo
+}
+else
+{
+    textWriter.WriteLine($"El palindromo mas largo fue: {bigpal}"); // en el caso opuesto agregaremos el palindromo que encontro al archivo de texto 
+}
 
-StreamWriter textWriter = File.CreateText(dir);
-textWriter.WriteLine(texto);
-textWriter.Close();
+textWriter.Close(); // cerraremos la escritura del archivo de texto
+
+// fin :)
+
