f090de2a9622cec03dc18a3d06543f7101a8a16b
GALLETAXO <galletoelpro@outlook.com>
2023-10-02 13:40:46 -0600

    codigoclase02/10/23arreglado

--- ProgramacionAvanzada/2doParcial/Libraries/Person.cs
+++ ProgramacionAvanzada/2doParcial/Libraries/Person.cs
@@ -1,26 +1,76 @@
-﻿namespace PA17F.shared;
-using System.Xml.Serialization;
+﻿﻿using System.Xml.Serialization; // [XMLAttributes]
+namespace PA17F.Shared;
 
 public class Person
 {
-   // Members
+    public delegate int DelegateWithMatchingSignature (string s);
+    public Person()
+    {
 
-   public Person()
-   {
+    }
+    public Person(decimal initialSalary)
+    {
+        Salary = initialSalary;
+    }
+    // members
+    // constant, read -onlys, events
+    /* Constant : Once declared and 
+    assigned, cannot be changed even
+    in runtime
 
-   }
-   public Person (decimal initialSalary)
-   {
-      Salary = initialSalary;
-   }
+    Read-Only : Similar to a constant
+    but you can change the value on runtime
+    using constructor
 
+    Event: Reference to another method
 
-   [XmlAttribute("fname")]
-   public string? Name;
-   [XmlAttribute("dob")]
-   public DateTime DateOfBirth;
-   protected decimal Salary {get;set;}
-   [XmlAttribute("lname")]
-   public string? LastName{get;set;}
-   public HashSet<Person>? Children {get; set;}
-}
+    Methods: Functions, Execute statements
+    Constructor : Executed when the NEW , allocates memory
+    
+    Property : 
+    member -> int number = 0;
+    public void setNumber (int number ) { this.number = number; }
+    public int get Number() { return this.number; }
+    string Name { get; set;} getter and setters
+
+    Indexers: []
+    Operators : + - => * / &
+    */
+    
+    // members
+    [XmlAttribute("fname")]
+    public string? Name;
+    [XmlAttribute("lname")]
+    public string? LastName { get; set; }
+    [XmlAttribute("dob")]
+    public DateTime DateOfBirth;
+    protected decimal Salary { get; set; }
+    public HashSet<Person>? Children { get; set; }
+    
+    // Delegates
+    public int MethodIWantToCall(string input)
+    {
+        return input.Length;
+    }
+
+    // 1st Step, Event Handler
+//     public delegate void EventHandler (object? sender, EventArgs e);
+
+//     // delegate field
+//     public EventHandler? Shout;
+//     // Data Field for delegate
+//     public int AngerLevel;
+//     public void Poke()
+//     {
+//         AngerLevel++;
+//         if(AngerLevel >= 3)
+//         {
+//             // if something is listening
+//             if(Shout != null)
+//             {
+//                 // then call the delegate
+//                 Shout(this, EventArgs.Empty);
+//             }
+//         }
+//     }
+ }
--- ProgramacionAvanzada/2doParcial/WorkingWithSerialization/Program.cs
+++ ProgramacionAvanzada/2doParcial/WorkingWithSerialization/Program.cs
@@ -1,95 +1,86 @@
-﻿using System.Xml.Serialization; // XmlSerializer
-using PA17F.shared;
+﻿﻿using System.Xml.Serialization; // XmlSerializer
+using PA17F.Shared;
 using static System.Environment;
 using static System.IO.Path;
-using static System.IO.Directory; 
 
 List<Person> people = new()
 {
-    new(3000M)
+    new(30000M)
     {
         Name = "Isaac",
         LastName = "Chavez",
         DateOfBirth = new (year: 2005, month: 03, day: 2)
     },
-
-    new(4000M)
+    new(40000M)
     {
         Name = "Carolina",
         LastName = "Prian",
         DateOfBirth = new(year: 2006, month: 05, day: 14)
-
     },
-
-    new(2000M)
+    new(20000M)
     {
         Name = "Samantha",
         LastName = "Valadez",
-        DateOfBirth = new(year: 2006, month: 05, day: 14)
-
+        DateOfBirth = new(year: 2005, month:02, day:20)
     },
-
     new(8000M)
     {
         Name = "Juan",
         LastName = "Torres",
-        DateOfBirth = new(year: 2001, month: 03, day: 25),
+        DateOfBirth = new(year: 2001, month:04, day:25),
         Children = new()
         {
             new(0M)
             {
                 Name = "Juanito",
                 LastName = "Cholico",
-                DateOfBirth = new(year: 2023, month: 10, day: 1)
-
+                DateOfBirth = new(year: 2023, month: 10, day:1)
             },
-            
             new(0M)
             {
                 Name = "Tamara",
                 LastName = "Cholico",
-                DateOfBirth = new(year: 2021, month: 9, day: 2)
-
-            },
+                DateOfBirth = new(year: 2021, month: 09, day:02)
+            }
         }
-
-    },
-
-    
+    }
 };
 
-
-
-// the xml serializer NEEDS to Know what 
-
-XmlSerializer xs = new (type:people.GetType());
-
-string path = Combine(CurremtDirectory, "people.xml");
-
-using(FileStream stream = File.Create(path))
+#region Serialize
+// The XML serialzer NEEDS to know what 
+// type of data is going to be serialized
+XmlSerializer xs = new(type: people.GetType());
+// create a file
+string path = Combine(CurrentDirectory, "people.xml");
+// Implicit Declaration
+// 3 birds 1 stone : 
+// 1: Take care of open on stream declaration
+// 2: When } gets hit , Close file is called
+// 3: Create file on declaration on Create(path)
+using (FileStream stream = File.Create(path))
 {
     // serialize the object
-    xs.serialize(stream,people);
+    xs.Serialize(stream, people);
 }
-
 WriteLine($"Written {new FileInfo(path).Length} bytes of XML to {path}");
 WriteLine();
+// READ
 WriteLine(File.ReadAllText(path));
+#endregion
 
-WriteLine();
-using (FileStream xmlLoad = File.open(path,FileMode.Open))
+#region Deserialize
+WriteLine("Deseriliaze people List");
+// Open file
+using (FileStream xmlLoad = File.Open(path,FileMode.Open))
 {
-    //deserialize
+    // Deserialize
     List<Person>? loadedPeople = xs.Deserialize(xmlLoad) as List<Person>;
     if(loadedPeople is not null)
     {
-        foreach(Person p in loadedPeople)
+        foreach (Person p in loadedPeople)
         {
-            WriteLine($"{p.Name} has {p.Children?.Count} children.");
+            WriteLine($"{p.Name} has {p.Children?.Count ?? 0} children.");
         }
-
-
-
     }
 }
-
+#endregion
