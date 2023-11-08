59c0110df08f4e259e14f53ff764a0d75d46d594
GALLETAXO <galletoelpro@outlook.com>
2023-11-08 15:19:21 -0600

    Practica2Exitosa

--- ProgramacionAvanzada/2doParcial/WorkingWithEFCore/Product.helper.cs
+++ ProgramacionAvanzada/2doParcial/WorkingWithEFCore/Product.helper.cs
@@ -3,35 +3,34 @@
 
 using System.Data.Common;
 using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
 using System.Text;
 using System.Text.RegularExpressions;
 
 namespace WorkingWithEFCore;
 
-public static partial class ProductExtensions
+public static partial class ProductExtensions // los metodos que hacen toda la magia :3
 {
-    public static int Moda(this List<Product> prod, int col)
+    public static int Moda(this List<Product> prod, int col) // aqui tenemos el metodo destinado a los valores Int 
     {
-        WriteLine("INT");
+        int[] prev = {0,0}; // aqui guardaremos el caracter provisional, en uno guardamos el numero y en el otro las veces que se repitio
+        int cuenta; // con esta variable llevamos la cuenta de el numero que estamos contando en ese momento
 
-        int[] prev = {0,0};
-        int cuenta;
-
-        List<int> datos = new();
-        for(int x = 0; x < prod.Count; x++)
+        List<int> datos = new(); // creamos una lista de Int donde guardaremos todos los valores de la columna que se necesite
+        for(int x = 0; x < prod.Count; x++) // iremos guardando valor por valor
         {
-            switch(col)
+            switch(col) // gracias a este switch determinamos de que columna estamos sacando los datos
             {
                 case 1:
                 {
                     
-                    datos.Add(prod[x].ProductId);
+                    datos.Add(prod[x].ProductId); // vamos a la posicion en la lista productos y obtenemos el valor en cuestion para agregarlo a nuestra lista de int 
                     break;
                 }
                 case 2:
                 {
                     
-                    datos.Add(prod[x].Stock);
+                    datos.Add(prod[x].Stock); 
                     break;
                 }
                 case 3:
@@ -42,7 +41,7 @@ public static partial class ProductExtensions
                 }
                 default:
                 {
-                    datos.Add(0);
+                    datos.Add(0); // caso malicioso en el que se llame mal la funcion :p
                     break;
                 }
             }
@@ -50,42 +49,41 @@ public static partial class ProductExtensions
 
         
 
-        for(int x = 0; x < datos.Count; x ++)
+        for(int x = 0; x < datos.Count; x ++) // ahora, aqui ocurre la magia, iremos contando cuantas veces aparece cada caracter en la lista
         {
-            cuenta = 0;
+            cuenta = 0; // reiniciamos la cuenta
             
-            for(int y = 0; y < datos.Count; y ++)
+            for(int y = 0; y < datos.Count; y ++) // comparamos un caracter con todo los demas de la lista
             {
 
-                if(datos[x] == datos[y])
+                if(datos[x] == datos[y]) // si encuentra una coinsidencia sumará la cuenta
                 {
                     cuenta++;
                 }
 
             }
 
-            if(cuenta > prev[1])
+            if(cuenta > prev[1]) // si al acabar con un caracter, la cuenta actual es mayor a la del caracter con mayor numero de apariciones, este se hara el nuevo caracter ms repetido
             {
                 prev[0] = datos[x];
                 prev[1] = cuenta;
             }
                      
         }
-        return prev[0];
+        return prev[0]; // al finalizar de todo ahora regresaremos el valor con mayor cantidad de coinsidencias
 
 
     }
 
 
-public static decimal? Moda(this List<Product> prod, decimal col)
+public static decimal? Moda(this List<Product> prod, decimal col) // funciona igual que el metodo anterior, solo que esta adaptado a a usar el tipo decimal
     {
-        WriteLine("Cost");
 
-        decimal?[] prev = {0,0};
+        decimal?[] prev = {0,0}; // cambiamos a un arreglo de decimales para alojar correctamente el valor de los costos
         int cuenta;
 
 
-        for(int x = 0; x < prod.Count; x ++)
+        for(int x = 0; x < prod.Count; x ++) // se hace el mismo proceso, el cambio mas drastico ademas del decimal, es que ahora obtenemos los valores directamente de la lista de producos 
         {
             cuenta = 0;
             
@@ -112,23 +110,20 @@ public static decimal? Moda(this List<Product> prod, decimal col)
     }
 
 
-    public static double Media(this List<Product> prod, int col)
+    public static double Media(this List<Product> prod, int col) // en este caso, hacemos algo similar, int col, nos funciona para diferenciar que esta funcion acepta int's 
     {
-        switch(col)
+        switch(col) // usamos col para  determinar la columna de la que sacamos los datos
         {
             case 1:
             {
-                WriteLine("ProdID");
-                return prod.Average(p=>p.ProductId);
+                return prod.Average(p=>p.ProductId); // simplemente sacamos el promedio de los datos de la columna en cuestion
             }
             case 2:
             {
-                WriteLine("Stock");
                 return prod.Average(p=>p.Stock);
             }
             case 3:
             {
-                WriteLine("CategID");
                 return prod.Average(p=>p.CategoryId);
             }
             default:
@@ -139,36 +134,33 @@ public static decimal? Moda(this List<Product> prod, decimal col)
         return 0;
     }
 
-    public static decimal? Media(this List<Product> prod, decimal col)
+    public static decimal? Media(this List<Product> prod, decimal col) // funciona de igual forma que la de int's, solo que ahora con decimal 
     {
-        WriteLine("Cost");
-        return prod.Average(p=>p.Cost);
+        return prod.Average(p=>p.Cost); // al no tener mas columnas con decimales, sabemos que nos referimos a cost, por lo que le sacamos directamente el promedio y listo
     }
 
-    public static decimal Mediana(this List<Product> prod, int col)
+    public static decimal Mediana(this List<Product> prod, int col) // para el caso de la mediana de int
     {
-        int med = 0;
-        int inter =prod.Count/2;
-        inter++;
+        int med = 0; // aqui guardaremos el resultado de la mediana
+        int inter =prod.Count/2; // obtenemos la posicion donde se aloja la mediana
+        inter++; // lo ajustamos un poco jsjs
         switch(col)
         {
-            case 1:
+            case 1: //  hacemos el mismo proceso en los 3 casos, lo que varia es la columna en cual nos basamos para ordenarlos y el dato de la mediana
             {
-                WriteLine("ProdID");
-                prod = prod.OrderBy(p=>p.ProductId).ToList();
-                if((prod.Count % 2) > 0)
+                prod = prod.OrderBy(p=>p.ProductId).ToList(); // ordenamos los datos en base a la columna
+                if((prod.Count % 2) > 0) // revisamos que tengamos una cantidad par o inpar de datos
                 {
-                    return prod[inter].ProductId;
+                    return prod[inter-1].ProductId; // en el caso de ser impar, solo obtenemos el valor que este justamente en medio de todos
                 }
                 else
                 {
-                    med = prod[prod.Count/2].ProductId + prod[(prod.Count/2) + 1].ProductId;
-                    return med/ 2;
+                    med = prod[prod.Count/2].ProductId + prod[(prod.Count/2) + 1].ProductId; // para el caso de que la cantidad sea par, obtendriamos un promedio de ambos datos que estan a la mitad
+                    return med/ 2; // regresarios dicho promedio 
                 }
             }
-            case 2:
+            case 2: // hacemos el mismo proceso pero para las otras columnas
             {
-                WriteLine("Stock");
                 prod = prod.OrderBy(p=>p.Stock).ToList();
                 if((prod.Count % 2) > 0)
                 {
@@ -180,9 +172,8 @@ public static decimal? Moda(this List<Product> prod, decimal col)
                     return med/ 2;
                 }
             }
-            case 3:
+            case 3: // hacemos el mismo proceso pero para las otras columnas
             {
-                WriteLine("CategID");
                 prod = prod.OrderBy(p=>p.CategoryId).ToList();
                 if((prod.Count % 2) > 0)
                 {
@@ -194,7 +185,7 @@ public static decimal? Moda(this List<Product> prod, decimal col)
                     return med/ 2;
                 }
             }
-            default:
+            default: // en caso de que se ingrese el dato de forma erronea solo se regresara un triste 0 :/
             {
                 break;
             }
@@ -205,9 +196,8 @@ public static decimal? Moda(this List<Product> prod, decimal col)
         return 0;
     }
 
-    public static decimal? Mediana(this List<Product> prod, decimal col)
+    public static decimal? Mediana(this List<Product> prod, decimal col) // el proceso es el mismo para los datos decimales, lo que cambia es que como sabes la columna, no hay necesidad de hacer un int
     {
-        WriteLine("Cost");
         int inter =prod.Count/2;
         inter++;
         prod = prod.OrderBy(p=>p.Cost).ToList();
@@ -223,131 +213,127 @@ public static decimal? Moda(this List<Product> prod, decimal col)
         }
     }
 
-    public static bool Moda(this List<Product> prod, bool col)
+    public static bool Moda(this List<Product> prod, bool col) // para obtener la moda hacemos lo siguiente
     {
-        WriteLine("Discontinued");
+        int t = 0, f = 0; // variables para contar las veces que aparece un falso o un verdadero
 
-        int t = 0, f = 0;
-
-        for(int x = 0; x < prod.Count; x ++)
+        for(int x = 0; x < prod.Count; x ++) // revisamos cada dato de la columna
         {
-            if(prod[x].Discontinued)
+            if(prod[x].Discontinued) // si este dato es verdadero sumaremos a la variable t
             {
                 t++;
             }
-            else
+            else // de no serlo, significa que es falso, sumamos a f 
             {
                 f++;
             }    
         }
 
-        if( t > f)
+        if( t > f) // en caso de que t sea mayor a f, regresaremos un true, ya que es el valor que mas se repite
         {
             return true;
         }
-        else
+        else // caso contrario regresaremos un false, por que es el que mas se repite
         {
             return false;
         }
         
     }
 
-    public static float Media(this List<Product> prod, bool col)
+    public static float Media(this List<Product> prod, bool col) // para la media de los bool
     {
-        WriteLine("Discontinued");
-        float f = 0, t = 0;
-        for(int x = 0; x < prod.Count; x ++)
+        float f = 0, t = 0; // tendremos dos variables, f, para la cantidad de valores falsos encontrados y t para el resultado jiji
+        for(int x = 0; x < prod.Count; x ++) // revisamos todos los valores de la columna
         {
-            if(prod[x].Discontinued == false)
+            if(prod[x].Discontinued == false) // si es falso, sumaremos a f 
             {
                 f++;
             }
         }
 
-        t = f * 100;
-        return t/prod.Count;
+        t = f * 100; // sacaremos el porcentaje de falsos en esta columna
+        return t/prod.Count; // segun la cantidad de datos 
         
     }
 
 
-    public static int Mediana(this List<Product> prod, bool col)
+    public static int Mediana(this List<Product> prod, bool col) // la mediana es algo curioso
     {
-        WriteLine("Discontinued");
-        int inter =prod.Count/2;
+        int inter =prod.Count/2; // determinamos la posicion del dato que este en el centro
         inter++;
-        prod = prod.OrderBy(p=>p.Discontinued).ToList();
-        if((prod.Count % 2) > 0)
+        prod = prod.OrderBy(p=>p.Discontinued).ToList(); // ordenamos los datos segun la columna discontinued
+        if((prod.Count % 2) > 0) // si tenemos una cantidad de datos impar 
         {
-            if(prod[inter].Discontinued)
+            if(prod[inter-1].Discontinued) // revisaremos el valor de el dato en mero en medio
             {
-                return 1;
+                return 1; // si es verdadero regresaremos un 1, indicando que es verdadero
             }
             else
             {
-                return 2;
+                return 2; // un dos si es falso 
             }
             
         }
         else
         { 
-            return 3;
+            return 3; // regresaremos un 3 si la cantidad es par, indicando que justo en medio puede hacer true y false al mismo tiempo 
         }
     }
 
 
-    public static char Moda(this List<Product> prod, string col)
+    public static char Moda(this List<Product> prod, string col) // aqui llega lo divertido :D
     {
-        string all = "";
+        string all = ""; // crearemos un string donde guardaremos TODAS las palabras en la columna deseada
 
-        switch(col)
+        switch(col) // determinamos de que columna obtendremos los datos
         {
-            case "N":
+            case "N": // N para nombres de productos
             {
-                foreach(var product in prod)
+                foreach(var product in prod)// revisaremos 1 por 1 cada valor de nombre
                 {
-                    all = all +product.ProductName;
+                    all = all +product.ProductName; // y lo sumaremos a nuestro string
                 }
 
                 break;
             }
-            case "C":
+            case "C": // C para Quantity jsjsjs
             {
-                foreach(var product in prod)
+                foreach(var product in prod) // revisamos dato por dato 
                 {
-                    all = all +product.Quantity;
+                    all = all +product.Quantity; // y lo agregamos al string
                 }
 
                 break;
             }
-            default:
+            default: // en caso de error no hay nada :p
             {
                 return ' ';
             }
 
         }
 
-        all = Regex.Replace(all.Normalize(NormalizationForm.FormD), @"[^a-zA-z0-9 ]+", "");
-        all = all.Replace(" ", "");
+        all = Regex.Replace(all.Normalize(NormalizationForm.FormD), @"[^a-zA-z0-9 ]+", ""); // le quitaremos tod rastro de acento o caracter malicioso
+        all = all.Replace(" ", ""); // erradicaremos todos los espacios
 
         
-        int cuenta, ant = 0;
-        char prev = ' ';
+        int cuenta, ant = 0; // esta parte se parece a los otros procedimientos para sacar la moda, cuenta es las veces que se repite el caracter actual y ant, las veces que se repitio el anterior mas repetido :p
+        char prev = ' '; // aqui guardaremos el caracter que mas se repita
 
-        for(int x = 0; x < all.Length; x ++)
+        for(int x = 0; x < all.Length; x ++) // revisamos caracter pro caracter 
         {
-            cuenta = 0;
+            cuenta = 0; // reiniciamos la cuenta 
             
-            for(int y = 0; y < all.Length; y ++)
+            for(int y = 0; y < all.Length; y ++) // revisaremos cada caracter con todos los demas
             {
 
-                if(all[x] == all[y])
+                if(all[x] == all[y]) // comparamos, si hay coinsidencia, la cuenta aumentara 
                 {
                     cuenta++;
                 }
 
             }
 
-            if(cuenta > ant)
+            if(cuenta > ant) // si nuestra cuenta actual es mayor que la anterior mas grande, estos valores pasaran a ser los mas repetidos 
             {
                 prev = all[x];
                 ant = cuenta;
@@ -358,17 +344,17 @@ public static decimal? Moda(this List<Product> prod, decimal col)
 
 
 
-        return prev;
+        return prev; // regresaremos el caracter mas repetido 
     }
 
-    public static double Media(this List<Product> prod, string col)
+    public static double Media(this List<Product> prod, string col) // la funcion de media para strings Omg
     {
 
-        double res = 0;
+        double res = 0; // aqui guardaremos el resultado y lo retornaremos
 
-        string all = "";
+        string all = ""; // nuestro string para guardar guardar todas las palabra
 
-        switch(col)
+        switch(col) // seguimos el mismo proceso de guardar en un string todas las palabras de una u otra columna
         {
             case "N":
             {
@@ -395,27 +381,27 @@ public static decimal? Moda(this List<Product> prod, decimal col)
 
         }
 
-        all = Regex.Replace(all.Normalize(NormalizationForm.FormD), @"[^a-zA-z0-9 ]+", "");
-        all = all.Replace(" ", "");
+        all = Regex.Replace(all.Normalize(NormalizationForm.FormD), @"[^a-zA-z0-9 ]+", ""); // tambien le quitamos cualquier acento o modificacion a los caracteres
+        all = all.Replace(" ", ""); // quitamos todos los espacios
 
-        for(int x = 0; x < all.Length; x ++)
+        for(int x = 0; x < all.Length; x ++) // ahora iremos caracter por caracter del string, sumaremos el valor de cada char a res 
         {
-            res = res + all[x];
+            res = res + all[x]; // :,)
         }
 
-        res = res/ all.Length;
+        res = res/ all.Length; // sacaremos la media segun la cantidad de caracteres del string
 
         return res;
 
     }
 
-    public static string Mediana(this List<Product> prod, string col)
+    public static string Mediana(this List<Product> prod, string col) // ahoratenemos la funcion para la mediana
     {
 
-        List<char> car = new();
-        string all = "";
+        List<char> car = new();  // es una mouse-querramienta que nos ayudara mas tarde >:), basicamente es una lista de char donde guardaremos un cada caracter de cada palabra de las columna deseada
+        string all = ""; // que por que con una lista? Por que estoy Loooooco., en fin, la misma variable para guardar todo el texto 
 
-        switch(col)
+        switch(col) // realizamos el mismo procedimiento de guardar todas las palabras de la columna en un solos string
         {
             case "N":
             {
@@ -443,31 +429,31 @@ public static decimal? Moda(this List<Product> prod, decimal col)
         }
 
         all = Regex.Replace(all.Normalize(NormalizationForm.FormD), @"[^a-zA-z0-9 ]+", "");
-        all = all.Replace(" ", "");
+        all = all.Replace(" ", ""); // le quitamos espacios,  acentos y todo lo que pueda estorbar
 
 
-        for(int x = 0; x < all.Length; x ++)
+        for(int x = 0; x < all.Length; x ++) // aqui esta la perversion, agregamos una por una todas las letras de todas las palabras de la columna a una lista de chars
         {
-            car.Add(all[x]);
+            car.Add(all[x]); // basicamente esto es con el fin de ordenar las letras jsjsjs
         }
 
-        car = car.Order().ToList();
+        car = car.Order().ToList(); // ordenamos la lista 
 
 
         
-        int inter =car.Count/2;
-        string res = " ";
+        int inter =car.Count/2; // hacemos un proceso similar a las otras medianas, obtenemos la posicion del valor que esta en medio 
+        string res = " "; // aqui guardaremos el resultado, es un string ya que como puede hacer dos valores enmendio, de esta forma mandamos los dos valores sin problemas
         inter++;
-        if((car.Count % 2) > 0)
+        if((car.Count % 2) > 0) // si la cantidad de valores es impar, mandaremos el valor que este justo al medio
         {
             res = res + car[inter];
         }
         else
         {
-            res = res + car[car.Count/2];
+            res = res + car[car.Count/2]; // caso contrario, mandaremos los 2 valores que estan al medio, esto para poder mandar caracteres y no tener que mandar un promedio de los valores de mabos caracteres :p 
             res = res + car[inter];
         }
-        return res;
+        return res; // regresamos el resultado en cuestion :p
 
     }
 
--- ProgramacionAvanzada/2doParcial/WorkingWithEFCore/Program.cs
+++ ProgramacionAvanzada/2doParcial/WorkingWithEFCore/Program.cs
@@ -5,27 +5,27 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.Net.Http.Headers;
 
-Northwind db = new();
+Northwind db = new(); 
 WriteLine($"Provider : {db.Database.ProviderName}");
 
 
-/*
-string input;
-int cuanta = 0;
-bool go = false;
+
+string input; // el dato que usaremos para determinar de cuanto en cuanto mostraremos las paginas
+int cuanta = 0; // los datos que se mostraran por paginas 
+bool go = false; // nos ayuda para crear un ciclo hasta que el usuario ingrese una s
 
 do
 {
 
-Console.Clear();
+Console.Clear();// borramos la consola para que sea mas entendible el asunto
 WriteLine("Escribe de cuanto en cuanto se mostrarán ej: 1, 5, 10, 25, 50, s para salir");
-input = ReadLine()!;
+input = ReadLine()!; // pedimos la opcion al usuario
 
-switch(input)
+switch(input) // vemos los casos segun la opcion
 {
     case "1":
     {
-        cuanta = 1;
+        cuanta = 1; // en estos casos, determinamos la cantidad de elementos por pagina
         go = false;
         break;
 
@@ -65,55 +65,51 @@ switch(input)
     }
     default:
     {
-        go = false;
+        go = false; // si se ingresa un dato que no este contemplado, el ciclo se repetirá
         break;
 
     }
 }
 if (go == false)
 {
-    paginator(cuanta);
+    paginator(cuanta); // llamamos a nuestra funcion que muestra los datos, le decimos de cuanto en cuantos los queremos
 }
 
-}while(go == false);
-
-
-*/
+}while(go == false); // salimos del ciclo cuando se ingrese una s
 
 
 
-            List<Product> prod = db.Products!.ToList();
 
 
-            WriteLine(prod.Moda(1));
-            WriteLine(prod.Moda(2));
-            WriteLine(prod.Moda(3));
-            WriteLine(prod.Moda(1M));
-            WriteLine(prod.Media(1));
+            List<Product> prod = db.Products!.ToList(); // obtenemos la tabla productos y la ingresamos a una lista
+
+            Console.Clear(); // borramos la consola para hacerlo mas entendible
+            WriteLine("ProductID"); // mostramos media, mediana y moda, de Product ID
+            WriteLine(prod.Media(1)); // le pasamos un entero para que use la funcion que es especificamente para enteros, podemos pasarle un 1, 2 o 3
+            WriteLine(prod.Mediana(1)); // si le pasamos un 1, significa que es ProductID, si es 2 es Stock, y si es 3, es CategoryID
+            WriteLine(prod.Moda(1)); // esto aplica para las funciones de Media mediana y moda de enteros, se diferencian ya que estas reciben un int
+            ReadLine();
+            WriteLine("Stock"); // mostramos media, mediana y moda, del Stock
             WriteLine(prod.Media(2));
+            WriteLine(prod.Mediana(2));
+            WriteLine(prod.Moda(2));
+            ReadLine();
+            WriteLine("CategoryID"); // mostramos media, mediana y moda, de Category ID
             WriteLine(prod.Media(3));
-            WriteLine(prod.Media(1M));
-            decimal? var = prod.Mediana(1);
-            WriteLine(var);
-            var = prod.Mediana(2);
-            WriteLine(var);
-            var = prod.Mediana(3);
-            WriteLine(var);
-            var = prod.Mediana(1M);
-            WriteLine(var);
+            WriteLine(prod.Mediana(3));
+            WriteLine(prod.Moda(3));
+            ReadLine();
+            WriteLine("Cost"); // mostramos media, mediana y moda, de Cost
+            WriteLine(prod.Media(1M)); // en el caso de cost, tenemos una funcion que recibe decimal, de esa forma diferenciamos que es para cost
+            WriteLine(prod.Mediana(1M)); // igualmente desde aqui le enviamos un decimal para que use la funcion que le corresponde
+            WriteLine(prod.Moda(1M));
+            ReadLine();
 
-            if(prod.Moda(true))
-            {
-                WriteLine("La moda es estar descontinuado");
-            }
-            else
-            {
-                WriteLine("La moda es  NO estar descontinuado");
-            }
 
             WriteLine($"En promedio el {prod.Media(true)}% de los productos no han sido descontinuados");
+            // aqui decido obtener el porcentaje de productos que han sido descontinuados, es lo mas cercano a una media que no sea simplemente la mitad de la cantidad de numeros
 
-            switch(prod.Mediana(true))
+            switch(prod.Mediana(true)) // aqui la funcion regresa un valor del 1 al 3, esto para determinar si 1, el valor en medio es Verdadero, 2 es falso y 3, es par y puede haber valores verdaderos y falsos
             {
                 case 1:
                 {
@@ -136,12 +132,29 @@ if (go == false)
                 }
             }
 
-            WriteLine($"El caracter que mas se repite en los nombres(moda) es: {prod.Moda("N")}");
-            WriteLine($"El caracter que mas se repite en la cantidad por unidad(moda) es: {prod.Moda("C")}");
-            WriteLine($"La media de todos los valores de todos los caracteres de los nombres es: {prod.Media("N")}");
-            WriteLine($"La media de todos los valores de todos los caracteres de la cantidad por unidades es: {prod.Media("C")}");
-            WriteLine($"La mediana de todos los caracteres de los nombres es: {prod.Mediana("N")}");
-            WriteLine($"La mediana de todos los caracteres de la cantidad por unidads es: {prod.Mediana("C")}");
+             WriteLine("Discontinued"); // mostramos media, mediana y moda, de Discontinued
+            if(prod.Moda(true)) // llamamos de esta forma a la funcion ya que la funcion devuelve el valor que mas se repitio, ya sea que fue falso o verdadero
+            {
+                WriteLine("La moda es estar descontinuado");
+            }
+            else
+            {
+                WriteLine("La moda es  NO estar descontinuado");
+            }
+            ReadLine();
+
+            WriteLine("ProductName"); // mostramos media, mediana y moda de Product name
+            WriteLine($"La media de todos los valores de todos los caracteres de los nombres es: {prod.Media("N")}"); // le pasamos un string para usar la funcion que trabaja con strings, nos devuelve el promedio de los valores  de cada caracter
+            WriteLine($"La mediana de todos los caracteres de los nombres es: {prod.Mediana("N")}"); // aqui nos devolvera el caracter que este justo al medio, ordenando segun el valor de cada caracter
+            WriteLine($"El caracter que mas se repite en los Nombres(moda) es: {prod.Moda("N")}"); // en este caso nos devuelve un char con la letra que mas se repitio
+            ReadLine();
+            WriteLine("Quantity");  // mostramos media, mediana y moda, de Quantity
+            WriteLine($"La media de todos los valores de todos los caracteres de la cantidad por unidades es: {prod.Media("C")}");// le pasamos un string para usar la funcion que trabaja con strings, nos devuelve el promedio de los valores  de cada caracter
+            WriteLine($"La mediana de todos los caracteres de la cantidad por unidads es: {prod.Mediana("C")}");// aqui nos devolvera el caracter que este justo al medio, ordenando segun el valor de cada caracter
+            WriteLine($"El caracter que mas se repite en la cantidad por unidad(moda) es: {prod.Moda("C")}");// en este caso nos devuelve un char con la letra que mas se repitio
+
+            
+
             
 
 
